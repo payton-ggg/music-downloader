11f6251a16bea7b585ec311334a36c75a223eadb
Platon <platon20081019@gmail.com>
2024-12-06 14:43:21 +0200

    asf

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,11 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+export const content = [
+  "./pages/**/*.{js,ts,jsx,tsx}",
+  "./components/**/*.{js,ts,jsx,tsx}",
+];
+export const theme = {
+  extend: {},
 };
+export const plugins = [];
